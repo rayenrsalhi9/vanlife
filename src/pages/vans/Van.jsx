d85fdc6355efaa928c70d475b6c6f7e4e57a6fa0
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-05 17:45:25 +0100

    fetched vans list from api.

--- src/pages/vans/Van.jsx
+++ src/pages/vans/Van.jsx
@@ -1,7 +1,24 @@
+import PropTypes from 'prop-types'
 
-
-export default function Van() {
+export default function Van({ van, vanClassName }) {
     return (
-        <h3>Van component goes here</h3>
+        <section className="van">
+            <img src={van.imageUrl} alt="van image" loading='lazy'/>
+            <div className="text">
+                <h3>{van.name}</h3>
+                <div className="price">
+                    <p>${van.price}</p>
+                    <span>/day</span>
+                </div>
+            </div>
+            <div className='type'>
+                <span className={vanClassName}>{van.type}</span>
+            </div>
+        </section>
     )
+}
+
+Van.propTypes = {
+    van: PropTypes.object,
+    vanClassName: PropTypes.string
 }
--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react'
+import clsx from 'clsx'
 import Van from './Van'
 import './Vans.css'
+import '../../server'
 
 export default function Vans() {
+
+    const [vans, setVans] = useState([])
+
+    useEffect(() => {
+        fetch('api/vans')
+            .then(data => data.json())
+            .then(res => setVans(res.vans))
+            .catch(error => console.error("Error fetching word:", error));
+    }, [])
+
+    const vansElements = vans.map(el => {
+        const vanClassName = clsx({
+            'simple': el.type === 'simple',
+            'rugged': el.type === 'rugged',
+            'luxury': el.type === 'luxury'
+        })
+        return <Van key={el.id} van={el} vanClassName={vanClassName}/>
+    })
+    
     return (
         <section className="vans">
-            <h1>Vans component goes here</h1>
-            <Van />
+            {vansElements}
         </section>
     )
 }
