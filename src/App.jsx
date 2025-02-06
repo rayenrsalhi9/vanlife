88d2c8fa35a1c59f6a902a553e972b452b9d782d
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-06 10:25:00 +0100

    enhanced navigation: relative paths & index routes.

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,18 @@ const App = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route element={<Layout />}>
-                    <Route path='/' element={<Home />}/>
-                    <Route path='/about' element={<About />}/>
-                    <Route path='/vans' element={<Vans />} />
-                    <Route path='/vans/:id' element={<VanDetails />} />
-                    <Route element={<HostLayout />}>    
-                        <Route path='/host' element={<Dashboard />}/>
-                        <Route path='/host/income' element={<Income />}/>
-                        <Route path='/host/vans' element={<HostVans />}/>
-                        <Route path='/host/reviews' element={<Reviews />}/>
+                <Route path='/' element={<Layout />}>
+                
+                    <Route index element={<Home />}/>
+                    <Route path='about' element={<About />}/>
+                    <Route path='vans' element={<Vans />} />
+                    <Route path='vans/:id' element={<VanDetails />} />
+
+                    <Route  path='host' element={<HostLayout />}>    
+                        <Route index element={<Dashboard />}/>
+                        <Route path='income' element={<Income />}/>
+                        <Route path='vans' element={<HostVans />}/>
+                        <Route path='reviews' element={<Reviews />}/>
                     </Route>
                 </Route>      
             </Routes>
