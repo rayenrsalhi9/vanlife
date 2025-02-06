bb87e9517e8575e4ce33f6c68ac5e88356bf640c
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-06 07:55:13 +0100

    enhanced the van details component.

--- src/pages/van-details/VanDetails.jsx
+++ src/pages/van-details/VanDetails.jsx
@@ -7,38 +7,43 @@ import './VanDetails.css'
 
 export default function VanDetails() {
     const params = useParams()
-    const [van, setVan] = useState({})
+    const [van, setVan] = useState(null)
 
     useEffect(() => {
         fetch(`/api/vans/${params.id}`)
         .then(data => data.json())
         .then(res => setVan(res.vans))
-    }, [])
+        .catch((error) => console.error(`Error fetching data: ${error}`))
+    }, [params.id])
 
     const className = clsx({
         'type': true,
-        'simple': van.type === 'simple',
-        'rugged': van.type === 'rugged',
-        'luxury': van.type === 'luxury',
+        'simple': van && van.type === 'simple',
+        'rugged': van && van.type === 'rugged',
+        'luxury': van && van.type === 'luxury',
     })
     
     return (
         <section className="details">
-            <Link to='/vans'>
-                <img src={leftArrow} alt="left arrow image" className='left-arrow' />
-                <p>Back to all vans</p>
-            </Link>
-            <img src={van.imageUrl} alt="van details image" className='details-img'/>
-            <div className="text">
-                <p className={className}>{van.type}</p>
-                <h1>{van.name}</h1>
-                <div className="price">
-                    <p>${van.price}</p>
-                    <span>/day</span>
-                </div>
-                <p>{van.description}</p>
-            </div>
-            <button className="rent-button">Rent this van</button>
+            {van ? (
+                <>
+                    <Link to='/vans'>
+                        <img src={leftArrow} alt="left arrow image" className='left-arrow' />
+                        <p>Back to all vans</p>
+                    </Link>
+                    <img src={van.imageUrl} alt="van details image" className='details-img'/>
+                    <div className="text">
+                        <p className={className}>{van.type}</p>
+                        <h1>{van.name}</h1>
+                        <div className="price">
+                            <p>${van.price}</p>
+                            <span>/day</span>
+                        </div>
+                        <p>{van.description}</p>
+                    </div>
+                    <button className="rent-button">Rent this van</button>
+                </>
+            ) : <h1 className='loading-message'>Loading...</h1>}
         </section>
     )
 }
