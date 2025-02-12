fb99bb1790cdd0bf241b4816e39af3a771f2bc90
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-12 19:12:29 +0100

    got rid of the loading message and its design.

--- src/pages/van-details/VanDetails.jsx
+++ src/pages/van-details/VanDetails.jsx
@@ -24,30 +24,26 @@ export default function VanDetails() {
     
     return (
         <section className="details">
-            {van ? (
-                <>
-                    <Link 
-                        to={location.state.search ? 
-                            `..?${location.state.search}` : 
-                            '..'} 
-                        relative='path'
-                    >
-                        <img src={leftArrow} alt="left arrow image" className='left-arrow' />
-                        <p>Back to {location.state.search ? `${location.state.type || 'all'}`:'all'} vans</p>
-                    </Link>
-                    <img src={van.imageUrl} alt="van details image" className='details-img'/>
-                    <div className="text">
-                        <p className={className}>{van.type}</p>
-                        <h1>{van.name}</h1>
-                        <div className="price">
-                            <p>${van.price}</p>
-                            <span>/day</span>
-                        </div>
-                        <p>{van.description}</p>
-                    </div>
-                    <button className="rent-button">Rent this van</button>
-                </>
-            ) : <h1 className='loading-message'>Loading...</h1>}
+            <Link 
+                to={location.state.search ? 
+                    `..?${location.state.search}` : 
+                    '..'} 
+                relative='path'
+            >
+                <img src={leftArrow} alt="left arrow image" className='left-arrow' />
+                <p>Back to {location.state.search ? `${location.state.type || 'all'}`:'all'} vans</p>
+            </Link>
+            <img src={van.imageUrl} alt="van details image" className='details-img'/>
+            <div className="text">
+                <p className={className}>{van.type}</p>
+                <h1>{van.name}</h1>
+                <div className="price">
+                    <p>${van.price}</p>
+                    <span>/day</span>
+                </div>
+                <p>{van.description}</p>
+            </div>
+            <button className="rent-button">Rent this van</button>
         </section>
     )
 }
