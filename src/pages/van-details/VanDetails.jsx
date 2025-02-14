1403dc88f6fb1885014570d539d62c70347a3097
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-14 22:00:48 +0100

    added defer, await and suspense to vanDetails section.

--- src/pages/van-details/VanDetails.jsx
+++ src/pages/van-details/VanDetails.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom'
+import { Suspense } from 'react'
+import { Link, defer, Await } from 'react-router-dom'
 import { useLocation, useLoaderData } from 'react-router-dom'
 import clsx from 'clsx'
 import leftArrow from '../../assets/left-arrow.svg'
@@ -7,43 +8,54 @@ import './VanDetails.css'
 
 // eslint-disable-next-line react-refresh/only-export-components
 export function vanDetailsLoader({ params }) {
-    return getVanDetails(params.id)
+    return defer({van: getVanDetails(params.id)})
 }
 
 export default function VanDetails() {
     const location = useLocation()
 
-    const van = useLoaderData()
-
-    const className = clsx({
-        'type': true,
-        'simple': van && van.type === 'simple',
-        'rugged': van && van.type === 'rugged',
-        'luxury': van && van.type === 'luxury',
-    })
+    const vanPromiseObject = useLoaderData()
     
     return (
         <section className="details">
-            <Link 
-                to={location.state.search ? 
-                    `..?${location.state.search}` : 
-                    '..'} 
-                relative='path'
-            >
-                <img src={leftArrow} alt="left arrow image" className='left-arrow' />
-                <p>Back to {location.state.search ? `${location.state.type || 'all'}`:'all'} vans</p>
-            </Link>
-            <img src={van.imageUrl} alt="van details image" className='details-img'/>
-            <div className="text">
-                <p className={className}>{van.type}</p>
-                <h1>{van.name}</h1>
-                <div className="price">
-                    <p>${van.price}</p>
-                    <span>/day</span>
-                </div>
-                <p>{van.description}</p>
-            </div>
-            <button className="rent-button">Rent this van</button>
+            <Suspense fallback={<h1 className='loading-message'></h1>}>
+                <Link 
+                    to={location.state.search ? 
+                        `..?${location.state.search}` : 
+                        '..'} 
+                    relative='path'
+                >
+                    <img src={leftArrow} alt="left arrow image" className='left-arrow' />
+                    <p>Back to {location.state.search ? `${location.state.type || 'all'}`:'all'} vans</p>
+                </Link>
+                
+                <Await resolve={vanPromiseObject.van}>
+                    {van => (
+                        <>
+                            <img src={van.imageUrl} alt="van details image" className='details-img'/>
+                            <div className="text">
+                                <p className={clsx({
+                                        'type': true,
+                                        'simple': van && van.type === 'simple',
+                                        'rugged': van && van.type === 'rugged',
+                                        'luxury': van && van.type === 'luxury',
+                                    })}
+                                >
+                                    {van.type}
+                                </p>
+                                <h1>{van.name}</h1>
+                                <div className="price">
+                                    <p>${van.price}</p>
+                                    <span>/day</span>
+                                </div>
+                                <p>{van.description}</p>
+                            </div>
+                        </>
+                    )}
+                </Await>
+
+                <button className="rent-button">Rent this van</button>
+            </Suspense>
         </section>
     )
 }
