b1bda5a6f9cfd0f66a23fc9c3b76a71600c6456a
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-02-15 13:22:50 +0100

    fetched data from firestore db instead of mirage, still user to fetch.

--- src/api.js
+++ src/api.js
@@ -1,29 +1,38 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBQcS91av3jWnhVGJ_6Ec_3Pc3YMBAJsqw",
+    authDomain: "vanlife-20b50.firebaseapp.com",
+    projectId: "vanlife-20b50",
+    storageBucket: "vanlife-20b50.firebasestorage.app",
+    messagingSenderId: "535691318715",
+    appId: "1:535691318715:web:625148d722c03e5d6ae09e"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, 'vans')
+
+// refactoring the fetching functions
+
 export async function getVans() {
-    const data = await fetch('/api/vans')
-    if (!data.ok) {
-        throw {
-            message: 'error fetching vans api',
-            statusText: data.statusText,
-            status: data.status
-        }
-    } else {
-        const res = await data.json()
-        return res.vans
-    }
+    const querySnapshot = await getDocs(vansCollectionRef)
+    const dataArr = querySnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+
+    return dataArr
 }
 
 export async function getVanDetails(id) {
-    const data = await fetch(`/api/vans/${id}`)
-    if (!data.ok) {
-        throw {
-            message: 'error fetching van details',
-            statusText: data.statusText,
-            status: data.status
-        }
-    } else {
-        const res = await data.json()
-        return res.vans
-    }
+    const docRef = doc(db, 'vans', id) //  (e.g. db > vans > 1)
+    const vanSnapshot = await getDoc(docRef)
+    const vanObject = {...vanSnapshot.data(), id: vanSnapshot.id}
+
+    return vanObject
 }
 
 export async function loginUser(creds) {
